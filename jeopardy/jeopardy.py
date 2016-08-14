634a902f705297c8a6dc7e562da11d420b0d20ad
Dane Johnson <dane_johnson@ymail.com>
2016-08-14 14:57:49 -0500

    implemented join and host socket layers

--- jeopardy/jeopardy.py
+++ jeopardy/jeopardy.py
@@ -3,13 +3,13 @@ import os
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, escape
 from flask_socketio import SocketIO, send, emit, join_room
 
-from utils import generate_room_name
+from utils import Room, Contestant, generate_room_name
 
 #Init application
 app = Flask(__name__)
 app.config.from_object(__name__)
 socketio = SocketIO(app)
-active_rooms = []
+active_rooms = {}
 
 #Load default config and override config from an environment variable
 app.config.update(dict(
@@ -28,11 +28,18 @@ def view(filename):
 #Backend
 @socketio.on('player joined')
 def add_player(data):
-	emit("accept player")
+	#This is firing twice per connection. Why?
+	print "adding player"
+	name = data['username']
+	room_code = data['room_code']
+	if room_code in active_rooms.keys():
+		active_rooms[room_code].add_contestant(Contestant(name))
+		# emit to all clients
+		emit('accept player', name, broadcast=True)
 @socketio.on('create room')
 def create_room():
 	room_name = generate_room_name(active_rooms)
-	active_rooms.append(room_name)
+	active_rooms[room_name] = Room(room_name)
 	emit('room created', room_name)
   
   
--- jeopardy/utils.py
+++ jeopardy/utils.py
@@ -1,5 +1,7 @@
 import string, random
+
 def generate_room_name(active_rooms):
+	"""Creates a new room matching regex [A-Z]{4}"""
 	has_name = False
 	while not has_name:
 		name = ''
@@ -8,3 +10,33 @@ def generate_room_name(active_rooms):
 		if not name in active_rooms:
 			has_name = True
 			return name
+class Room:
+	"""Holds the data concearning a single game"""
+	def __init__(self, room_code):
+		self._room_code = room_code
+		self._contestants = {}
+		self._has_host = False
+		self._has_contestants = False
+
+	def add_contestant(self, contestant_name):
+		self._contestants[contestant_name] = Contestant(contestant_name)
+		if not self._has_contestants:
+			self._has_contestants = True
+	def add_host(self):
+		self._has_host = True
+	@property
+	def is_ready(self):
+		return self._has_host and self._has_contestants
+
+
+class Contestant:
+	"""Holds the data concearning a single contestant"""
+	def __init__(self, name):
+		self._name = name
+		self._money = 0
+	def __iadd__(self, new_money):
+		"""Overridden to allow for quick adding of cash"""
+		self._money += new_money
+	def __isub__(self, new_money):
+		"""Overridden to allow for quick removal of cash"""
+		self._money -= new_money
